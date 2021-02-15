c365115b42b18db966941c7df576f393686f55c0
Diogo Monteiro <diogo@blockthrough.com>
2021-02-15 23:08:54 +0000

    adding patch scroll-back-mouse-altscree

--- config.h
+++ config.h
@@ -207,6 +207,12 @@ ResourcePref resources[] = {
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
+
+  // Custom
+	{ XK_ANY_MOD,           Button4,        kscrollup,      {.i = 1}, },
+	{ XK_ANY_MOD,           Button5,        kscrolldown,    {.i = 1}, },
+
+  // Default
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
